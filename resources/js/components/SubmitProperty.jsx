dbedd3dc43884c52ce2c58612d75416e04521264
mosesalcantara <mosesalcantara123@gmail.com>
2024-07-11 16:37:36 +0800

    submit prop multi page

--- resources/js/components/SubmitProperty.jsx
+++ resources/js/components/SubmitProperty.jsx
@@ -1,13 +1,80 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function SubmitProperty() {
-    const prevTab = () => {
-        console.log('prev')
+    const [tab, setTab] = useState(1)
+
+    const Tab1 = () => {
+        return (
+            <>
+                <div className='tab'>
+
+                    <h1 className='heading'>Personal Information</h1>
+                    <hr />
+
+                    <div className="row">
+                        <div className="col-sm-6 mb-4">
+                            <div className="form-group">
+                                <label htmlFor="">Firstname</label>
+                                <input type="text" className='form-control' placeholder='Juan' />
+                            </div>
+                        </div>
+                        <div className="col-sm-6 mb-4">
+                            <div className="form-group">
+                                <label htmlFor="">Lastname</label>
+                                <input type="text" className='form-control' placeholder='Dela Cruz' />
+                            </div>
+                        </div>
+                        <div className="col-sm-6 mb-4">
+                            <div className="form-group">
+                                <label htmlFor="">Mobile Number</label>
+                                <input type="text" className='form-control' placeholder='(+63) 9924401097' />
+                            </div>
+                        </div>
+                        <div className="col-sm-6 mb-4">
+                            <div className="form-group">
+                                <label htmlFor="">Email</label>
+                                <input type="email" className='form-control' placeholder='juandelacruz@gmail.com' />
+                            </div>
+                        </div>
+
+                        <div className="col-sm-12 mb-4">
+                            <div className="form-group">
+                                <label htmlFor="">Address</label>
+                                <input type="text" className='form-control' placeholder='Makati City, Philippines' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="form-button">
+                    <button type='button' id='nextBtn' className='btn btn-form-next me-2' onClick={() => {setTab(tab+1)}}>Next</button>
+                </div>
+            </>
+        )
     }
 
-    const nextTab = () => {
-        console.log('next')
+    const Tab2 = () => {
+        return (
+            <>
+                <div className='tab'>
+                    <h1 className='heading'>Personal Information</h1>
+                    <hr />
+
+                    <div className="col-sm-6 mb-4">
+                        <div className="form-group">
+                            <label htmlFor="">Email</label>
+                            <input type="email" className='form-control' placeholder='juandelacruz@gmail.com' />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="form-button">
+                    <button type='button' id='prevBtn' className='btn btn-form-prev me-2' onClick={() => {setTab(tab-1)}}>Previous</button>
+                    <button type='button' id='submitBtn' className='btn btn-form-next me-2'>Submit</button>
+                </div>
+            </>
+        )
     }
 
     return (
@@ -16,63 +83,9 @@ function SubmitProperty() {
             <div className="submitProperty">
                 <div className="col-md-12 col-panel">
                     <div className="submitHeading">
-                        <div className='tab' id="tab-1">
-
-                            <h1 className='heading'>Personal Information</h1>
-                            <hr />
-
-                            <div className="row">
-                                <div className="col-sm-6 mb-4">
-                                    <div className="form-group">
-                                        <label htmlFor="">Firstname</label>
-                                        <input type="text" className='form-control' placeholder='Juan' />
-                                    </div>
-                                </div>
-                                <div className="col-sm-6 mb-4">
-                                    <div className="form-group">
-                                        <label htmlFor="">Lastname</label>
-                                        <input type="text" className='form-control' placeholder='Dela Cruz' />
-                                    </div>
-                                </div>
-                                <div className="col-sm-6 mb-4">
-                                    <div className="form-group">
-                                        <label htmlFor="">Mobile Number</label>
-                                        <input type="text" className='form-control' placeholder='(+63) 9924401097' />
-                                    </div>
-                                </div>
-                                <div className="col-sm-6 mb-4">
-                                    <div className="form-group">
-                                        <label htmlFor="">Email</label>
-                                        <input type="email" className='form-control' placeholder='juandelacruz@gmail.com' />
-                                    </div>
-                                </div>
-
-                                <div className="col-sm-12 mb-4">
-                                    <div className="form-group">
-                                        <label htmlFor="">Address</label>
-                                        <input type="text" className='form-control' placeholder='Makati City, Philippines' />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='tab' id="tab-2">
-                            <h1 className='heading'>Personal Information</h1>
-                            <hr />
-
-                            <div className="col-sm-6 mb-4">
-                                <div className="form-group">
-                                    <label htmlFor="">Email</label>
-                                    <input type="email" className='form-control' placeholder='juandelacruz@gmail.com' />
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div className="form-button">
-                            <button id='prevBtn' className='btn btn-form-prev me-2' onClick={prevTab}>Previous</button>
-                            <button id='nextBtn' className='btn btn-form-next me-2' onClick={nextTab}>Next</button>
-                        </div>
+                        {
+                            tab == 1 ? <Tab1></Tab1> : <Tab2></Tab2>
+                        }
 
                     </div>
                 </div>
